b225d0392d5eac89a91a5ca61d39f6256049d3d1
andrew <andrew@cloud-latitude.com>
2023-09-10 15:35:59 +0800

    different users

--- src/WalletController.spec.ts
+++ src/WalletController.spec.ts
@@ -15,20 +15,37 @@ describe('WalletController', () => {
     });
 
     it('get balance', () => {
-        expect(walletController.getBalance()).toBe(0)
+        walletController.create(1);
+        expect(walletController.getBalance(1)).toBe(0)
     });
 
 
     it('save 100', () => {
-        walletController.save(100);
-        expect(walletController.getBalance()).toBe(100);
+        walletController.create(1);
+        walletController.save(1, 100);
+        expect(walletController.getBalance(1)).toBe(100);
     });
 
 
     it('save 100 twice', () => {
-        walletController.save(100);
-        walletController.save(100);
-        expect(walletController.getBalance()).toBe(200);
+
+        walletController.create(1);
+
+        walletController.save(1, 100);
+        walletController.save(1, 100);
+        expect(walletController.getBalance(1)).toBe(200);
+    });
+
+    it('different users', () => {
+
+        walletController.create(1);
+        walletController.create(2);
+
+        walletController.save(1, 100);
+        walletController.save(2, 200);
+
+        expect(walletController.getBalance(1)).toBe(100);
+        expect(walletController.getBalance(2)).toBe(200);
     });
 
 
--- src/WalletController.ts
+++ src/WalletController.ts
@@ -1,11 +1,33 @@
-export class WalletController {
-    private amount = 0;
+class User {
+    userId: number;
+    amount: number;
 
-    save(amount: number) {
-        this.amount += amount;
+    constructor(userId: number, amount: number) {
+        this.userId = userId;
+        this.amount = amount;
+
     }
 
-    getBalance(): any {
-        return this.amount;
+}
+
+export class WalletController {
+    private amount = 0;
+    private mapper: { [key: number]: User } = {};
+
+    create(userId: number) {
+        this.mapper[userId] = new User(userId, 0);
+    }
+
+
+    save(userId: number, amount: number) {
+        const user = this.mapper[userId];
+
+        user.amount += amount;
+
+        this.mapper[userId] = user;
+    }
+
+    getBalance(userId: number): number {
+        return this.mapper[userId].amount;
     }
 }
