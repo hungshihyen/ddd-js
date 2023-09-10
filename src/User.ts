99611cbd87681cc03bb6ef40a8df198ed243ccdd
andrew <andrew@cloud-latitude.com>
2023-09-10 15:42:55 +0800

    extract User

---
+++ src/User.ts
@@ -0,0 +1,10 @@
+export class User {
+    userId: number;
+    amount: number;
+
+    constructor(userId: number, amount: number) {
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+}
