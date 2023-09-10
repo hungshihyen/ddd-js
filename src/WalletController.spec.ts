6acb9579baf87f76965650eea15ca2e0815e472c
andrew <andrew@cloud-latitude.com>
2023-09-10 15:23:43 +0800

    get balance

--- src/WalletController.spec.ts
+++ src/WalletController.spec.ts
@@ -1,3 +1,9 @@
+class WalletController {
+    getBalance(): any {
+        return 0;
+    }
+}
+
 describe('WalletController', () => {
     // get balance
     // save 100
@@ -7,6 +13,10 @@ describe('WalletController', () => {
     // bad format amount
     // user not found
     // user locked
+
+    it('get balance', () => {
+        expect(new WalletController().getBalance()).toBe(0)
+    });
 
 
 });
