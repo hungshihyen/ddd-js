6f78fd89cfaeefcfe5529de2e490274b0c509db2
andrew <andrew@cloud-latitude.com>
2023-09-13 13:13:27 +0800

    different users

--- src/WalletController.spec.ts
+++ src/WalletController.spec.ts
@@ -1,12 +1,19 @@
 class WalletController {
     amount: number = 0;
+    mapper: { [key: number]: number } = {};
 
-    getBalance(): any {
-        return this.amount;
+    createUser(userId: number) {
+        this.mapper[userId] = 0;
     }
 
-    save(arg0: number): any {
-        this.amount += arg0;
+    getBalance(userId: number): any {
+        // return this.amount;
+        return this.mapper[userId];
+    }
+
+    save(userId: number, amount: number): any {
+        // this.amount += amount;
+        this.mapper[userId] += amount;
 
     }
 }
@@ -16,28 +23,47 @@ describe('WalletController', () => {
     it('should save successful', () => {
 
         const walletController = new WalletController();
-        walletController.save(100);
+        walletController.createUser(1);
+        walletController.save(1, 100);
 
-        expect(walletController.getBalance()).toBe(100);
+        expect(walletController.getBalance(1)).toBe(100);
 
     });
 
     it('save 200', () => {
 
         const walletController = new WalletController();
-        walletController.save(200);
+        walletController.createUser(1);
+        walletController.save(1, 200);
 
-        expect(walletController.getBalance()).toBe(200);
+        expect(walletController.getBalance(1)).toBe(200);
 
     });
 
     it('save twice', () => {
 
         const walletController = new WalletController();
-        walletController.save(100);
-        walletController.save(100);
+        walletController.createUser(1);
 
-        expect(walletController.getBalance()).toBe(200);
+        walletController.save(1, 100);
+        walletController.save(1, 100);
+
+        expect(walletController.getBalance(1)).toBe(200);
+
+    });
+
+    it('different users', () => {
+
+        const walletController = new WalletController();
+
+        walletController.createUser(1);
+        walletController.createUser(2);
+
+        walletController.save(1, 100);
+        walletController.save(2, 100);
+
+        expect(walletController.getBalance(1)).toBe(100);
+        expect(walletController.getBalance(2)).toBe(100);
 
     });
 
