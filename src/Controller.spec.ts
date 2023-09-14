e27a3f2ce209c5a0d705cc3cb75cad5315be5a0a
andrew <andrew@cloud-latitude.com>
2023-09-14 10:14:50 +0800

    extract WalletController

--- src/Controller.spec.ts
+++ src/Controller.spec.ts
@@ -1,18 +1,20 @@
 import { WalletController } from './WalletController';
 
 describe('Controller', () => {
+    let walletController: WalletController;
+    beforeEach(() => {
+        walletController = new WalletController();
+    });
 
     it('should save money ok', () => {
-        const walletController = new WalletController();
         walletController.mapper[1] = 0;
-       
+
         walletController.save(1, 1000);
 
         expect(walletController.getBalance(1)).toBe(1000);
     });
 
     it('should save 2000', () => {
-        const walletController = new WalletController();
 
         walletController.mapper[1] = 0;
 
@@ -22,7 +24,6 @@ describe('Controller', () => {
     });
 
     it('should save 1000 twice', () => {
-        const walletController = new WalletController();
 
         walletController.mapper[1] = 0;
 
@@ -33,7 +34,6 @@ describe('Controller', () => {
     });
 
     it('different users', () => {
-        const walletController = new WalletController();
 
         walletController.mapper[1] = 0;
         walletController.mapper[2] = 0;
