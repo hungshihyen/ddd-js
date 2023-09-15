dcd6ee52febef02501a44b5f3a43b9cd4503fd19
andrew <andrew@cloud-latitude.com>
2023-09-16 00:54:10 +0800

    remove unused repository

--- src/WalletController.ts
+++ src/WalletController.ts
@@ -1,16 +1,15 @@
-import { WalletRepository } from './WalletRepository';
 import { CreateUserService } from './CreateUserService';
 import { GetBalanceService } from './GetBalanceService';
 import { SaveMoneyService } from './SaveMoneyService';
 
 export class WalletController {
-    walletRepository: WalletRepository;
     createUserService: CreateUserService;
     private saveMoneyService: SaveMoneyService;
     private getBalanceService: GetBalanceService;
 
-    constructor(createUserService: CreateUserService, getBalanceService: GetBalanceService, saveMoneyService: SaveMoneyService) {
-        this.walletRepository = new WalletRepository();
+    constructor(createUserService: CreateUserService,
+                getBalanceService: GetBalanceService,
+                saveMoneyService: SaveMoneyService) {
         this.createUserService = createUserService;
         this.getBalanceService = getBalanceService;
         this.saveMoneyService = saveMoneyService;
